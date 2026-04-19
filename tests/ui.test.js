2ef822856fb324a6e198b0a124428298f8f5a719
Sumit <sumit9229krsingh@gmail.com>
2026-04-20 03:16:35 +0530

    Fix: correct path to index.html in test file

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf8');
 
 document.body.innerHTML = html;
 
